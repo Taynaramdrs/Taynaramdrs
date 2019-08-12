adfd00e1af75ac01047581b4138fdfed912f2863
Taynaramdrs <jemessontaynara@gmail.com>
2019-08-12 14:45:45 -0300

    teste

---
+++ Mylib.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+void menu( void ){
+    system("Clear");
+    
+    printf("_ _ _ _ _SIG-Fantasy Locadora de Roupas e Fantasias_ _ _ _ _\n\n");
+    printf("1 - Cadastrar usuário\n ");
+    printf("2 - Cadastrar roupas ou fantasias\n");
+    printf("3 - Consultar roupas ou fantasias\n");
+    printf("4 - Efetuar aluguel\n");
+    printf("5 - Sobre\n");
+}
